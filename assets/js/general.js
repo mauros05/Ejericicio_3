a49c9cfd1ef6243a2d89e53fa6953b6b7c6c1593
mauros05 <emaurogtz@hotmail.es>
2022-12-13 10:19:14 -0600

    Add refactor to ajax function

---
+++ assets/js/general.js
@@ -0,0 +1,44 @@
+function peticionAjax(datos){
+    return new Promise(function(resolve,reject){
+        $.ajax({
+            url: datos.url,
+            data: datos.data,
+            type: "GET",
+            dataType: "json",
+            success: function (res) {
+                resolve(res)
+            },
+
+            error: function(xhr, status){
+                reject(status)
+            },
+
+            complete: function (xhr, status) {  
+                console.log("Peticion correcta");
+            }
+        })
+    })
+}
+
+function peticionAjax2(datos){
+    var respuesta;
+    $.ajax({
+        async: false,
+        url: datos.url,
+        data: datos.data,
+        type: "GET",
+        dataType: "json",
+        success: function (res) {
+            respuesta = res
+        },
+
+        error: function(xhr, status){
+            console.log("Algo salio mal")
+        },
+
+        complete: function (xhr, status) {  
+            console.log("Peticion correcta");
+        }
+    })
+    return respuesta
+}
--- assets/js/misSolicitudes.js
+++ assets/js/misSolicitudes.js
@@ -1,49 +1,21 @@
 $(document).ready(function(){
     var obj = {};
-    $(".btn-ver").click(function(){
+    $(".btn-ver").click(async function(){
         var idSolicitud = $(this).data("id");
         obj.data   = {idSolicitud: idSolicitud, ac:"buscarSolicitud"};
         obj.url    = "compras.php";
         obj.accion = "verSolicitud"   
-        peticionAjax(obj);
+       var res = await peticionAjax(obj);
+            $("#folio").val(res.folio);
+            $("#fecha").val(res.fecha_creacion);
+            $("#cantidad").val(res.cantidad);
+            $("#descripcion").val(res.descripcion);
+            $("#urgencia").val(res.id_urgencia);
+            $("#producto").val(res.producto);
+            $("#codigo-producto").val(res.codigo_producto);
+            $("#categoria").val(res.categoria);
+            $("#status").val(res.status);
+            $("#status").addClass(res.color);
+            $("#modalVerSolicitud").modal("show");
     })
 });
-
-function peticionAjax (datos){
-    $.ajax({
-        url: datos.url,
-            data: datos.data,
-            type: "GET",
-            dataType: "json",
-        success: function (res) {
-            switch(datos.accion){
-
-            case "verSolicitud":
-                $("#folio").val(res.folio);
-                $("#fecha").val(res.fecha_creacion);
-                $("#cantidad").val(res.cantidad);
-                $("#descripcion").val(res.descripcion);
-                $("#urgencia").val(res.id_urgencia);
-                $("#producto").val(res.producto);
-                $("#codigo-producto").val(res.codigo_producto);
-                $("#categoria").val(res.categoria);
-                $("#status").val(res.status);
-                $("#status").addClass(res.color);
-                $("#modalVerSolicitud").modal("show");
-            break;
-
-            case "guardarSolicitud":
-            break;
-            }
-
-        },
-
-        error: function(xhr, status){
-            console.log("Algo salio mal :(");
-        },
-
-        complete: function (xhr, status) {  
-            console.log("Peticion correcta");
-        }
-    });
-}
